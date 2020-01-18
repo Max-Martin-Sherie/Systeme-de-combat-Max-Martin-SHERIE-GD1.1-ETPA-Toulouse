06dcac01522748dc96ab774085b75b96924afabe
Max-Martin-Sherie <59953355+Max-Martin-Sherie@users.noreply.github.com>
2020-01-18 21:46:32 +0100

    Update Systeme_De_Combat_Max-Martin_SHERIE_GD_1_1.c

--- Systeme_De_Combat_Max-Martin_SHERIE_GD_1_1.c
+++ Systeme_De_Combat_Max-Martin_SHERIE_GD_1_1.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+//Declaration des points de vies
+int PDV=100;
+
+
+//Fonction d'une attaque simple
+int attaqueSimple(int viesCible) {
+	viesCible=viesCible-10;
+	
+	return viesCible;
+}
+
 int main(){
+	
+	printf("vous avez %d points de vies\n", PDV);
+	printf("l'ennemi utulise attaque simple\n");
+	
+	PDV=attaqueSimple(PDV);
+	
+	printf("vous avez %d points de vies\n", PDV);
 	return 0;
 }
