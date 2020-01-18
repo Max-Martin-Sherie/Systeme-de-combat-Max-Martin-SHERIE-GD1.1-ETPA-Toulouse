83ba325d769863f51963d947be821f62ecc4e3f9
Max-Martin-Sherie <59953355+Max-Martin-Sherie@users.noreply.github.com>
2020-01-18 23:42:06 +0100

    Update Systeme_De_Combat_Max-Martin_SHERIE_GD_1_1.c

--- Systeme_De_Combat_Max-Martin_SHERIE_GD_1_1.c
+++ Systeme_De_Combat_Max-Martin_SHERIE_GD_1_1.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 //Declaration des points de vies
 int PDVJoueur=100;
 int PDVMonstre=200;
+int PMJoueur=5;
+int PMMonstre=3;
+int empoisonementJoueur=0;
+int empoisonementMonstre=0;
 int defense=0;
 int numAttaque;
 
 //Fonction d'une attaque simple
-int attaqueSimple(int viesCible, int protection) {
-	if(protection==1){
+int attaqueSimple(int viesCible) {
+	if(defense==1){
 		viesCible=viesCible-5;
 	}else{
 		viesCible=viesCible-20;
@@ -17,32 +23,63 @@ int attaqueSimple(int viesCible, int protection) {
 	return viesCible;
 }
 
+//Fonction d'une instance d'un empoisonnement
+int empoisonement(int viesCible) {
+	
+	viesCible=viesCible-5;
+	return viesCible;
+}
+
 int main(){
-	printf("vous rencontrex un ennemis!\n Il est vert, hideux, et ressemble a Valentin\n Le combat commence!\n\n");
+	printf("\n Vous rencontrez un ennemis!\n Il est vert, hideux, et ressemble a Valentin\n Le combat commence!\n\n");
 	
 	while ((PDVJoueur>1) && (PDVMonstre>1)){
 		printf("c'est a vous de jouer\n");
-		printf("(1)attaquer (2)se defendre\n");
+		printf("Vous avez %d PM\n",PMJoueur);
+		printf("(1)attaque (2)se defense (3)poison\n");
 		
 		scanf("%d", &numAttaque);
 		
-		while(numAttaque != 1 && numAttaque != 2){
-			printf("Il n'y a pas d'attaque numero %d ! \n",numAttaque);
+		while(numAttaque != 1 && numAttaque != 2 && (numAttaque==3 && PMJoueur<5)){
+			if(PMJoueur<5 && numAttaque==3){
+				printf("vous n'avez pas assez de PM pour lancer ce sort");
+			}else{
+				printf("Il n'y a pas d'attaque numero %d ! \n",numAttaque);
+			}
 			scanf("%d", &numAttaque);
 		}
 		if(numAttaque==1){
-			PDVMonstre=attaqueSimple(PDVMonstre,defense);
+			PDVMonstre=attaqueSimple(PDVMonstre);
 			printf("Vous frappez l'ennemi d'un glorieux coup de sabre !\n");
+			defense=0;
 		}else if(numAttaque==2){
-			printf("vous vous retraitez derriere votre bouclier en mythril!\n");
+			printf("Vous vous retraitez derriere votre bouclier en mythril!\n");
+			defense=1;
+		}else if(numAttaque==3){
+			printf("Vous jetez un sort de poisson pourris sur l'ennemi!\n");
+			PMJoueur=PMJoueur-5;
+			empoisonementMonstre=3;
+			defense=0;
+		}
+		if(empoisonementMonstre>0){
+			empoisonementMonstre--;
+			printf("Le monstre empoissone perd 5 points de vie\n");
+			PDVMonstre=empoisonement(PDVMonstre);
+		}
+		
+		printf("Il reste %d points de vie au monstre\n \n",PDVMonstre);
+		
+		numAttaque = rand()%2;
+		
+		if(numAttaque==0){
+		PDVJoueur=attaqueSimple(PDVJoueur);
+		printf("Le Monstre attaque d'un coup de pate hideuse!\n");
+		defense=0;
+		}else if(numAttaque==1){
+			printf("Le monstre de protege\n");
 			defense=1;
 		}
-		printf("Il reste %d Points de vie au monstre\n \n",PDVMonstre);
 		
-		
-		PDVJoueur=attaqueSimple(PDVJoueur,defense);
-		defense=0;
-		printf("Le Monstre attaque d'un coup de pate hideuse!\n");
 		printf("Il vous reste %d Points de vie\n \n",PDVJoueur);
 	}
 	if(PDVJoueur>0){
